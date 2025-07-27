bb9d8b7f80de352e0efccad70e263a9f11e7dfe2
0vinci <wrobelish@gmail.com>
2025-07-27 20:02:58 +0200

    cors url added

--- src/Shared/Recommendations.Shared.Infrastructure/Extensions.cs
+++ src/Shared/Recommendations.Shared.Infrastructure/Extensions.cs
@@ -61,7 +61,7 @@ public static class Extensions
         {
             options.AddPolicy("DefaultCorsPolicy", policy =>
             {
-                policy.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
+                policy.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://recommendations-git-main-0vincis-projects.vercel.app")
                       .AllowAnyMethod()
                       .AllowAnyHeader()
                       .AllowCredentials();
