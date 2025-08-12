f280340bcafeaba6ad426054a5b353a43708f06c
0VINCI <101973216+0VINCI@users.noreply.github.com>
2025-08-12 22:23:34 +0200

    structure fix

---
+++ src/ContentBased/Recommendations.ContentBased.Core/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Recommendations.ContentBased.Core.Commands;
+using Recommendations.ContentBased.Core.ModuleApi;
+using Recommendations.ContentBased.Core.Queries;
+using Recommendations.ContentBased.Shared;
+
+namespace Recommendations.ContentBased.Core;
+
+public static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services)
+    {
+        services.AddSingleton<IContentBasedModuleApi, ContentBasedModuleApi>();
+        services.AddCommands();
+        services.AddQueries();
+        //services.AddPostgres();
+
+        return services;
+    }
+}
